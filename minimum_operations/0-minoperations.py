8176f139b8e9e71a44ad126d4be6af4b886cc189
RamziHkiri <6257@holbertonstudents.com>
2024-01-24 14:59:02 +0100

    minimum number of operation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""  
-method that calculates the fewest number of operations needed
+""" method that calculates the fewest number of operations needed
 to result in exactly n H characters in the file"""
 
 
 def minOperations(n):
-    """method min operation """
-    
-    start='H'
-    newString=start
-    num = 0
-    while (len(newString) < n ):
-        num = num + 1
-        newString= newString + start
+    """ method min operation """
 
-        if ( len(newString)> n):
+    start = 'H'
+    newString = start
+    num = 0
+    while(len(newString) < n):
+        num = num + 1
+        newString = newString + start
+
+        if(len(newString) > n):
             return 0
 
-        if(n % len(newString)==0):
+        if(n % len(newString) == 0):
             start = newString
             num = num + 1
 
