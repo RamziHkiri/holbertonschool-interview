f0a58478e0ccb945c7d5b194b64dac5527251ff8
RamziHkiri <6257@holbertonstudents.com>
2024-01-24 15:13:22 +0100

    fix documentation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,7 +3,14 @@
 
 
 def minOperations(n):
-    """ method min operation needed to get 'h' n time """
+    """ method min operation needed to get 'h' n time
+     parameters:
+        n [int]: number of copies of the character desired
+
+    returns:
+        the minimum number of operations needed to result in n characters
+        or 0 if n is impossible to achieve"""
+
 # initialize the start file
     start = 'H'
     # the new string after every paste operation
@@ -20,6 +27,7 @@ def minOperations(n):
             return 0
         # if n divided by the length of the new string we will copy all
         # and increment the number of operation by one
+
         if(n % len(newString) == 0):
             start = newString
             num = num + 1
