059935f51fd83cf7f9b54ef29d7559cace12fff7
RamziHkiri <6257@holbertonstudents.com>
2024-01-24 15:08:03 +0100

    minimum number of operation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -5,17 +5,22 @@ to result in exactly n H characters in the file"""
 
 def minOperations(n):
     """ method min operation """
-
+# initialize the start file
     start = 'H'
+    # the new string after every paste operation
     newString = start
+    # number of operation
     num = 0
+
     while(len(newString) < n):
+        # increment the number of peration by 1 and paste
         num = num + 1
         newString = newString + start
 
         if(len(newString) > n):
             return 0
-
+        # if n divided by the length of the new string we will copy all
+        # and increment the number of operation by one
         if(n % len(newString) == 0):
             start = newString
             num = num + 1
