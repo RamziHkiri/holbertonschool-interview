29e408fc0660f2b6c3d9a9a4743f5c5a48b5d533
RamziHkiri <6257@holbertonstudents.com>
2024-01-24 15:39:24 +0100

    fix doc

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -12,23 +12,16 @@ def minOperations(n):
         Returns:
             number of operations (Copy & Paste) to reach n Hs
     """
-
-    # initialize the start file
+    
     start = 'H'
-    # the new string after every paste operation
     newString = start
-    # number of operation
     num = 0
     while(len(newString) < n):
-        # increment the number of peration by 1 and paste
         num = num + 1
         newString = newString + start
         if(len(newString) > n):
             return 0
-        # if n divided by the length of the new string we will copy all
-        # and increment the number of operation by one
         if(n % len(newString) == 0):
             start = newString
             num = num + 1
-
     return num
