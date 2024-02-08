9859cd5783b00a22ce8ee88a330757f41b6d9a84
RamziHkiri <6257@holbertonstudents.com>
2024-02-08 13:24:49 +0100

    fuction that fedine if a single list is palindrome or no

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int is_palindrome(listint_t **head){
+   listint_t *current = *head;  
+
+    int length = 0;
+    int array[999];
+    int i;
+    while (current != NULL)
+	{
+		array[length] = current->n;
+		current = current->next;
+		length++;
+	}
+
+    for (i = 0; i < (length / 2); i++)
+	{
+		if (array[i] != array[length - i -1])
+			return (0);
+	}
+	return (1);
+}
