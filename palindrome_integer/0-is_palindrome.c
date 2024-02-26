95d47a972c4449fe1d0326722b1baeac0c423d1e
RamziHkiri <6257@holbertonstudents.com>
2024-02-26 14:41:22 +0100

    test if long int is palindrome

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "palindrome.h"
+#include <string.h>
+/**
+ * is_palindrome - test if a unsigned long int is palindrome or not
+ * @n: unsigned long integer
+ * Return: 1 if n is palindrome 0 otherwise
+ */
+
+int is_palindrome(unsigned long n)
+{
+char str[20];
+int left = 0, right = 0;
+sprintf(str, "%ld", n);
+right = strlen(str) - 1;
+while (left <= right && str[left] == str[right] )
+{
+left++;
+right--;
+}
+if (left > right)
+{
+    return(1);
+}
+else
+{
+return (0);
+}
+}
