c81630ab0706befff245919cfc605fea926502b7
RamziHkiri <6257@holbertonstudents.com>
2024-03-06 19:01:32 +0100

    0-validate_utf8.py

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -2,10 +2,13 @@
 """utf8 validation methode"""
 
 
+from codecs import decode
+
+
 def validUTF8(data):
     """ determines if a given data set represents a valid UTF-8 encoding."""
-    for x in data:
-        if x < 0 or x >= 256:
-            return False
-
-    return True
+    try:
+        decoded_data = decode(bytes(data), 'utf-8')
+        return True
+    except ValueError:
+        return False
