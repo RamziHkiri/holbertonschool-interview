eb7ef3ae763a2b65a556f9dd7cea6b48d08686fd
RamziHkiri <6257@holbertonstudents.com>
2024-03-19 03:35:13 +0100

    fix

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -2,53 +2,55 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "slide_line.h"
-
-int slide_line(int *line, size_t size, int direction){
-
-    int new_line[size];
-    int indice;
-    
-
-    for(size_t i = 0; i < size ; i++){
-        new_line[i] = 0;
-    }
-  
-    if(direction == SLIDE_LEFT){
-        indice = 0;
-        for(size_t i = 0; i < size ; i++){
-            if(line[i] != 0){
-                if(line[i] != new_line[indice]){
-                    new_line[indice] = line[i];
-                }
-                else{
-                    new_line[indice] += line[i];
-                    indice++;
-                } 
-            }   
-        }
-        for(size_t i = 0; i < size ; i++){
-        line[i] = new_line[i];
-        }
-        return 1;
-    }
-    else if(direction == SLIDE_RIGHT){
-        indice = size - 1;
-        for(size_t i = size ; i > 0 ; i--){
-            if(line[i - 1] != 0){
-                if(line[i-1] != new_line[indice]){
-                    new_line[indice] = line[i-1];
-                }
-                else{
-                    new_line[indice] += line[i-1];
-                    indice--;
-                }
-            }   
-        }
-        for(size_t i = 0; i < size ; i++){
-        line[i] = new_line[i];
-        }
-        return 1 ;
-    }else{
-        return 0;
-    }
+int slide_line(int *line, size_t size, int direction)
+{
+int new_line[size];
+int indice;
+size_t i;
+for (i = 0; i < size ; i++)
+new_line[i] = 0;
+if (direction == SLIDE_LEFT)
+{
+indice = 0;
+for (i = 0; i < size ; i++)
+{
+if (line[i] != 0)
+{
+if (line[i] != new_line[indice])
+new_line[indice] = line[i];
+else
+{
+new_line[indice] += line[i];
+indice++;
+}
+}
+}
+for (i = 0; i < size ; i++)
+line[i] = new_line[i];
+return (1);
+}
+else if (direction == SLIDE_RIGHT)
+{
+indice = size - 1;
+for (i = size ; i > 0 ; i--)
+{
+if (line[i - 1] != 0)
+{
+if (line[i-1] != new_line[indice])
+new_line[indice] = line[i-1];
+else
+{
+new_line[indice] += line[i-1];
+indice--;
+}
+}   
+}
+for (size_t i = 0; i < size ; i++)
+line[i] = new_line[i];
+return (1) ;
+}
+else
+{
+return (0);
+}
 }
