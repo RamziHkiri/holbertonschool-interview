af9a945242eca6d271e80c87b5446eb6555c2c35
RamziHkiri <6257@holbertonstudents.com>
2024-07-24 11:04:09 +0200

    making change function

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-""" make change function"""
+
 
 def makeChange(coins, total):
-    """make changes function"""
     
     if total <= 0:
         return 0
     coins.sort(reverse=True)
     num = 0
     i = 0
-    while total // coins[i] != 0:
+    while total // coins[i] >= 1:
         num += total // coins[i]
         total %= coins[i]
         i += 1
         if total == 0 :
             return num
-
     return -1
-           
         
     
