d0117bf7392c9ecee0049a6dee24372364e6b551
RamziHkiri <6257@holbertonstudents.com>
2024-07-24 14:29:45 +0200

    0-merge_sort.c

---
+++ merge_sort/0-merge_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * merge_sort - sorting function using the merge sort
+ * @array: array of integer
+ * @size: size of array
+ * return:sorted list
+ */
+
+void merge_sort(int *array, size_t size)
+{
+    
+}
