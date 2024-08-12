9d36cd3b198fb3db3a8647204e9f3421d8b4e910
RamziHkiri <6257@holbertonstudents.com>
2024-08-12 12:41:08 +0200

    wildcmp

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -4,7 +4,6 @@
  * wildcmp - check the code for Holberton School students.
  * @s1: pointer to the first string.
  * @s2: pointer to the second string.
- * 
  * Return: 1 if the strings can be considered identical, otherwise return 0.
  */
 int wildcmp(char *s1, char *s2)
