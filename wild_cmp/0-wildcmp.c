92c694bd3522b3cd84f9b2440b3d737fdca2c4f0
RamziHkiri <6257@holbertonstudents.com>
2024-08-12 12:38:13 +0200

    wildcamp

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * wildcmp - check the code for Holberton School students.
+ * @s1: pointer to the first string.
+ * @s2: pointer to the second string.
+ * 
+ * Return: 1 if the strings can be considered identical, otherwise return 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int tmp = 0;
+
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*s1 != '\0')
+			tmp = wildcmp(s1 + 1, s2);
+		return (wildcmp(s1, s2 + 1) || tmp);
+	}
+	return (0);
+}
