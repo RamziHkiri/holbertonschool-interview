6765e79978152211dde964db4a339df81b14858e
RamziHkiri <6257@holbertonstudents.com>
2024-08-12 13:35:49 +0200

    0-island_perimeter.py

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""Returns the perimeter of the island described in grid"""
+"""returns the perimeter of the island described in grid"""
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid
+    """returns the perimeter of a given grid."""
+    val = 0
+    rows = len(grid)
+    cols = len(grid[0])
 
-    Args:
-        grid: list of integers
+    def is_water(x, y):
+        return x < 0 or y < 0 or x >= rows or y >= cols or grid[x][y] == 0
 
-    Returns: perimeter of island described in grid
-    """
-    c = 0
-    length = len(grid) - 1
-    width = len(grid[0]) - 1
+    for column in range(rows):
+        for row in range(cols):
+            if grid[column][row] == 1:
+                val += is_water(column - 1, row)
+                val += is_water(column + 1, row)
+                val += is_water(column, row + 1)
+                val += is_water(column, row - 1)
 
-    for i, r in enumerate(grid):
-        for j, n in enumerate(r):
-            if n == 1:
-                if i == 0 or grid[i - 1][j] != 1:
-                    c += 1
-                if j == 0 or grid[i][j - 1] != 1:
-                    c += 1
-                if j == width or grid[i][j + 1] != 1:
-                    c += 1
-                if i == length or grid[i + 1][j] != 1:
-                    c += 1
-    return c
+    return val
